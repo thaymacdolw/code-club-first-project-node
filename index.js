586484ed5e9599153d4e956f94d361a2a7a78c58
Thayna Mac Dolw <thaymacdolw@gmail.com>
2024-05-21 19:40:23 +0200

    changing port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const uuid = require('uuid')
 const cors = require ('cors')
 
-const port = 3001;
+const port = 3002;
 const app = express();
 app.use(express.json());
 app.use(cors());
